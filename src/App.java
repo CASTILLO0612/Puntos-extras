4e81f1f271a662e3d9b628484c35b5454e3416db
angel.flores894@std.uni.edu.ni <angelfloresgranada7@gmail.com>
2024-04-27 09:39:09 -0600

    TAREA 5 PUNTOS MEJORAS EN SOUT

--- src/App.java
+++ src/App.java
@@ -21,7 +21,6 @@ public class App {
                 System.out.println("Ingrese valores correctos");
                 leer.nextLine();
             }
-
         } while (num1 == num2 || num1 == num3 || num2 == num3);
 
         //Suma de los dos numeros mas pequenos, resta de los mayores e impresion del mayor
@@ -35,9 +34,8 @@ public class App {
              }
 
             System.out.println("El mayor es " + num1);
-    
         }
-            if (num2>num3 && num2>num1) {
+         if (num2>num3 && num2>num1) {
             System.out.println("La suma de los numeros menores es: " +(num3+num1) );
             if (num2>num1 && num1>num3) {
                 System.out.println("La resta de los mayores es: "+(num2-num1) );     
@@ -48,7 +46,6 @@ public class App {
             System.out.println("El mayor es: "+num2);
 
         }
-
         if (num3>num1 && num3>num2) {
             System.out.println("La suma de los menores es: "+ (num1+num2)) ;
             if (num3>num1 && num1>num2) {
